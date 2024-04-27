76d8919203b885187115a33476a04239b3d26d64
Mo Hank <hank@Hanks-MacBook-Pro.local>
2024-04-27 10:19:11 +0800

    allow markdown file to auto wrap

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,10 +1,13 @@
 require("lvim.lsp.manager").setup "marksman"
 
+-- allow word wrap
+vim.opt.wrap = true
+
 require("glow").setup({
-  glow_path = "", -- will be filled automatically with your glow bin in $PATH, if any
+  glow_path = "",                -- will be filled automatically with your glow bin in $PATH, if any
   install_path = "~/.local/bin", -- default path for installing glow binary
-  border = "shadow", -- floating window border config
-  style = "dark", -- filled automatically with your current editor background, you can override using glow json style
+  border = "shadow",             -- floating window border config
+  style = "dark",                -- filled automatically with your current editor background, you can override using glow json style
   pager = false,
   width = 80,
   height = 100,
